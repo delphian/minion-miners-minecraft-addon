207fef89789c6401602d15ab618f62cdc060317f
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2019-10-30 22:43:43 -0700

    Refactoring code.

--- minion_miners_behavior/scripts/server/server.js
+++ minion_miners_behavior/scripts/server/server.js
@@ -1,9 +1,61 @@
 const aServerSystem = server.registerSystem(0, 0);
 
+// Namespace.
+MinionMiners = {};
+
+// Time of day (in minecraft) class.
+MinionMiners.timeOfDay = function(realLifeMinutesInDay) {
+    var config = {
+        tickIncrement: null,
+    };
+    // Maintain current time in discrete units.
+    var time = {
+        hour: 0,
+        minute: 0,
+        second: 0,
+        tick: 0
+    };
+    /**
+     * Return current time object.
+     */
+    this.getTime = function() {
+        return JSON.parse(JSON.stringify(time));
+    };
+    /**
+     * Calculate Minecraft daytime tick value based on time object.
+     */
+    this.getTick = function() {
+        return Math.floor(time.tick);
+    };
+    /**
+     * Set time object based on minecraft daytime tick value (0-24000).
+     * 
+     * @param {int} tick Tick of current daytime as found in minecraft.
+     */
+    this.setTick = function(tick) {
+        time.hour = Math.floor(time.tick / (20 * 60 * 60));
+        time.minute = Math.floor((time.tick - (time.hour * 1000)) / (20 * 60));
+        time.second = 0;
+        time.tick = tick;
+        return this;
+    };
+    /**
+     * Increment the time by a single tick (1/20 of a second)
+     */
+    this.incrementTick = function() {
+        time.tick += config.tickIncrement;
+        if (time.tick >= (20 * 60 * 60 * 24)) {
+            time.tick = 0;
+        }
+        this.setTick(time.tick);
+    };
+    config.tickIncrement = 24000 / (realLifeMinutesInDay * 60 * 20);
+};
+
 /**
  * Base Minion Miners controller.
  */
-MinionMiners = function(injectServer) {
+MinionMiners.Core = function(injectServer) {
     var self = this;
     var minecraftServer = injectServer;
     var modules = [];
@@ -24,7 +76,9 @@ MinionMiners = function(injectServer) {
         }
         initialized = true;
     };
-    // Update is called every tick
+    /**
+     * Called by Minecraft each tick (20 times per second).
+     */
     this.update = function() {
         if (initialized == true) {
             for (i = 0; i < modules.length; i++) {
@@ -46,60 +100,73 @@ MinionMiners = function(injectServer) {
  * 
  * Controls the sun, moon, and keeping track of the yearly/monthly/daily calendar.
  */
-MMCalendar = function() {
-    var config = {
-        rlMinutesInDay: 1
-    };
-    var ticksInHour = 1000;
+MinionMiners.Calendar = function(realLifeMinutesInDay) {
     var mm = null;
-    var timeOfDay = {
-        hour: 0,
-        minute: 0,
-        second: 0,
-        tick: 0
+    var time = null;
+    var ready = null;
+    /**
+     * Sync time object to current minecraft daytime tick (0 - 24000).
+     * 
+     * @param {function} callback Callback to invoke with new time object when sync is complete.
+     */
+    this.syncTime = function(callback) {
+        this.getMinecraftDaytimeTick((tick) => {
+            time.setTick(tick);
+            if (callback) {
+                callback(time);
+            }
+        });
     };
-    var syncing = false;
-    var ready = false;
-    this.initialize = function(injectMinionMiners) {
-        mm = injectMinionMiners;
-        // Convert minutes in day to number of ticks in an hour
+    this.initialize = function(injectCore) {
+        mm = injectCore;
+        // Disable normal daylight cycle
         mm.getServer().executeCommand("/gamerule dodaylightcycle false", () => {});
-        ticksInHour = config.rlMinutesInDay * 50;
     };
+    /**
+     * Called by core each script tick (20 times per second).
+     */
     this.update = function() {
-        if (mm.isReady()) {
-            if (ready == false && syncing == false) {
-                this.syncTimeOfDay();
-            }
-            if (ready == true) {
-                timeOfDay.tick++;
-                // Recalculate time more often when minutes in day is low, less often when minutes in day is high.
-                if (timeOfDay.tick % config.rlMinutesInDay == 0) {
-                    timeOfDay.hour = Math.floor(timeOfDay.tick / ticksInHour);
-                    if (timeOfDay.hour >= 24) {
-                        timeOfDay.hour = 0;
-                        timeOfDay.tick = 0;
-                    }
-                    timeOfDay.minute = Math.floor((timeOfDay.tick - (timeOfDay.hour * ticksInHour)) / (ticksInHour / 60));
-                    mm.getServer().executeCommand("/time set " + Math.floor((timeOfDay.hour * 1000) + (timeOfDay.minute * (1000 / 60))), () => {});
-                }
-            }
+        if (mm.isReady() && ready == null) {
+            ready = false;
+            this.syncTime(() => {
+                ready = true;
+            });
+        }
+        if (mm.isReady() && ready) {
+            time.incrementTick();
+            this.setMinecraftDaytimeTick(time.getTick());
         }
     };
-    this.syncTimeOfDay = function() {
-        syncing = true;
+    /**
+     * Set current minecraft daytime tick.
+     * 
+     * This will update the position of the sun, moon, and stars.
+     * 
+     * @param {int} tick New value of daytime (0-24000)
+     */
+    this.setMinecraftDaytimeTick = function(tick) {
+        mm.getServer().executeCommand("/time set " + tick, () => {});
+    };
+    /**
+     * Get current minecraft daytime tick.
+     * 
+     * @param {function} callback Function to execute with result
+     */
+    this.getMinecraftDaytimeTick = function(callback) {
         mm.getServer().executeCommand("/time query daytime", (resultData) => {
             var n = resultData.data.body.split(" ");
-            timeOfDay.tick = n[n.length - 1];
-            syncing = false
-            ready = true;
-        });  
+            var tick = n[n.length - 1];
+            if (callback) {
+                callback(tick);
+            }
+        });
     };
+    time = new MinionMiners.timeOfDay(realLifeMinutesInDay);
 };
 
-
-let mm = new MinionMiners(aServerSystem);
-mm.registerModule(new MMCalendar())
+let mm = new MinionMiners.Core(aServerSystem);
+// Initialize calendar with a day cycle of 1 minute
+mm.registerModule(new MinionMiners.Calendar(1))
 
 let mmWeather = {};
 mmWeather.Data = {};
